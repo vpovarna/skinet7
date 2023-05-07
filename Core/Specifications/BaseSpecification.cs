55e49886ae0f374e9798e4ee22751afac72c06f3
Vlad Povarna <povarna@adobe.com>
2023-05-07 11:09:53 +0300

    Adding Sorting functionality on the listProducts api

--- Core/Specifications/BaseSpecification.cs
+++ Core/Specifications/BaseSpecification.cs
@@ -4,6 +4,8 @@ namespace Core.Specifications;
 
 public class BaseSpecification<T> : ISpecification<T>
 {
+    private Expression<Func<T, object>> _orderByDescending;
+
     protected BaseSpecification()
     {
     }
@@ -15,9 +17,24 @@ public class BaseSpecification<T> : ISpecification<T>
 
     public Expression<Func<T, bool>> Criteria { get; }
     public List<Expression<Func<T, object>>> Includes { get; } = new();
+    
+    public Expression<Func<T, object>> OrderBy { get; private set; }
+    
+    public Expression<Func<T, object>> OrderByDescending { get; private set; }
+
 
     protected void AddInclude(Expression<Func<T, object>> includeExpression)
     {
         Includes.Add(includeExpression);
+    }
+
+    protected void AddOrderBy(Expression<Func<T, object>> orderByExpression)
+    {
+        OrderBy = orderByExpression;
+    }
+    
+    protected void AddOrderByDescending(Expression<Func<T, object>> orderByDescendingExpression)
+    {
+        OrderByDescending = orderByDescendingExpression;
     }
 }
--- Core/Specifications/ISpecification.cs
+++ Core/Specifications/ISpecification.cs
@@ -5,6 +5,10 @@ namespace Core.Specifications;
 public interface ISpecification<T>
 {
     Expression<Func<T, bool>> Criteria { get; }
-    
-    List<Expression<Func<T, object>>> Includes { get;  }
+
+    List<Expression<Func<T, object>>> Includes { get; }
+
+    Expression<Func<T, Object>> OrderBy { get; }
+
+    Expression<Func<T, Object>> OrderByDescending { get; }
 }
